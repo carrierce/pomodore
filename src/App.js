3a57e7cb2a12c43557ff31a3c1d27dd625f98fe7
Charles E Carrier <charles.e.carrier@gmail.com>
2019-09-25 15:22:26 +0800

    working on pomodorable

--- src/App.js
+++ src/App.js
@@ -1,37 +1,164 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import moment from "moment";
 
 class App extends Component {
   state = {
-    now: null,
-    then: null,
-    countdown: null
+    countDown: 1800,
+    atWork: false,
+    buttonTitle: "Start",
+    pomodoreCount: 0,
+    pomodoreTaskDescription: "",
+    pomodoreTaskArray: []
   };
 
-  componentDidMount() {
-    this.timer();
-  }
+  audio = new Audio(
+    "https://interactive-examples.mdn.mozilla.net/media/examples/t-rex-roar.mp3"
+  );
 
-  countdown = () => {
-    return moment().format();
+  handleChange = event => {
+    this.setState({
+      pomodoreTaskDescription: event.target.value
+    });
   };
 
-  timer = () => {
-    setInterval(() => {
+  handleSubmit = event => {
+    this.setState({
+      pomodoreTaskArray: [
+        ...this.state.pomodoreTaskArray,
+        this.state.pomodoreTaskDescription
+      ],
+      pomodoreTaskDescription: ""
+    });
+    event.preventDefault();
+  };
+
+  countDown = () => {
+    if (this.state.countDown > 0) {
       this.setState({
-        now: moment().format("hh:mm:ss"),
-        then: moment()
-          .add(30, "minutes")
-          .format("hh:mm:ss")
+        countDown: this.state.countDown - 1
       });
+    } else {
+      this.setState({
+        pomodoreCount: this.state.pomodoreCount + 1,
+        atWork: false,
+        countDown: 1800
+      });
+      clearInterval(this.intervalId);
+    }
+    if (this.state.countDown === 300) {
+      this.audio.play();
+    }
+  };
+
+  startTime = () => {
+    clearInterval(this.intervalId);
+    this.setState({
+      atWork: true
+    });
+    this.intervalId = setInterval(() => {
+      this.countDown();
     }, 1000);
   };
 
+  pauseTime = () => {
+    clearInterval(this.intervalId);
+    this.setState({
+      atWork: false
+    });
+  };
+
+  skipBreak = () => {
+    this.setState({
+      countDown: 1800,
+      pomodoreCount: this.state.pomodoreCount + 1
+    });
+    this.startTime();
+  };
+
+  resetTimer = () => {
+    this.setState({
+      countDown: 1800,
+      atWork: false
+    });
+    clearInterval(this.intervalId);
+  };
+
+  formatTimer = timeOff => {
+    return (
+      <p>
+        {(this.state.countDown - timeOff) / 60 >= 10
+          ? Math.floor((this.state.countDown - timeOff) / 60)
+          : "0" + Math.floor((this.state.countDown - timeOff) / 60)}
+        :
+        {(this.state.countDown - timeOff) % 60 >= 10
+          ? (this.state.countDown - timeOff) % 60
+          : "0" + ((this.state.countDown - timeOff) % 60)}
+      </p>
+    );
+  };
+
+  taskArray = () => {
+    let taskArray = this.state.pomodoreTaskArray;
+    let tasks = taskArray.map((task, index) => {
+      return (
+        <div key={index}>
+          {index + 1} {task}
+        </div>
+      );
+    });
+    return <div>{tasks}</div>;
+  };
+
   render() {
-    console.log(this.countdown());
-    return <div>{this.state.then}</div>;
+    return (
+      <div className="App">
+        <div className="header">
+          <h1>Pomadorable</h1>
+        </div>
+        {this.state.atWork && (
+          <button className="btn" onClick={this.pauseTime}>
+            Pause
+          </button>
+        )}
+        {!this.state.atWork && (
+          <button className="btn" onClick={this.startTime}>
+            Start
+          </button>
+        )}
+        <button className="btn" onClick={this.resetTimer}>
+          Reset
+        </button>
+        {this.state.countDown < 300 && (
+          <button className="btn" onClick={this.skipBreak}>
+            Skip Break
+          </button>
+        )}
+        <div className="timeDisplay">
+          <h2>
+            {this.state.countDown > 300 ? "Count Down:" : "Break Time Baby!"}
+          </h2>
+          {this.state.countDown > 300
+            ? this.formatTimer(300)
+            : this.formatTimer(0)}
+          <h2>Today's Pomadorables</h2>
+          <p>{this.state.pomodoreCount}</p>
+        </div>
+        {/* <div className="pomodoroTask">
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Task:
+              <input
+                type="text"
+                value={this.state.pomodoreTaskDescription}
+                onChange={this.handleChange}
+              />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+        <div>{this.taskArray()}</div> */}
+      </div>
+    );
   }
 }
 
